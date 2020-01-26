a353ddff60cb6261822ffcb3b0d1418fcf8db3fd
joel <joel@6clicks.io>
2020-01-26 17:12:15 +1100

    Initial check-in of API code

---
+++ Api/Controllers/VersionController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class VersionController : ControllerBase
+    {
+        [HttpGet]
+        public string GetVersion()
+        {
+            return "1.0.0.0";
+        }
+    }
+}
