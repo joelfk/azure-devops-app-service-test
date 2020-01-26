53c914356f0a862330ebdb7582113cd161a154bf
joel <joel@6clicks.io>
2020-01-26 17:57:53 +1100

    Adding failing tests

---
+++ Api.Test/VersionController.Tests.cs
@@ -0,0 +1,19 @@
+﻿using Api.Controllers;
+using Xunit;
+
+namespace Api.Test
+{
+    public class VersionControllerTests
+    {
+        public class When_GetVersion_Is_Called
+        {
+            [Fact]
+            public void Then_The_Correct_Version_Is_Returned()
+            {
+                var controller = new VersionController();
+
+                Assert.Equal("Incorrect", controller.GetVersion());
+            }
+        }
+    }
+}
